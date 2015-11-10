d84971241138a238f00df7ce9c8e168086a33f1a
murat-aka <murat-aka@hotmail.com>
2015-11-10 23:57:57 +0000

    solution to normilezed sense function quiz

--- week 1/Normalized Sense Function quiz.py
+++ week 1/Normalized Sense Function quiz.py
@@ -14,5 +14,9 @@ def sense(p, Z):
     for i in range(len(p)):
         hit = (Z == world[i])
         q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
+    s = sum(q)
+    
+    for i in range(len(p)):
+        q[i] = q[i]/s
     return q
 print sense(p,Z)
